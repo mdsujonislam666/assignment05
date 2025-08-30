dd8dc70fcea3fdfe6d006644a88af251ddc185e0
mdsujonislam666c <mdsujonislam666>
2025-08-30 00:01:11 +0000

    this code for card button name and number used to call history

--- scripts.js/heart.js
+++ scripts.js/heart.js
@@ -3,47 +3,60 @@ const storeData = [];
 // heart section
 
 const heartButton = document.getElementsByClassName('heart-icon');
-for(let j =0; j<heartButton.length; j++ ){
+for (let j = 0; j < heartButton.length; j++) {
     heartButton[j].addEventListener("click", function (e) {
-    e.preventDefault();
-    const heartCount = document.getElementById('heart-Count').innerText;
-    const heartCountNumber = parseInt(heartCount);
-    const totalHeardCount = heartCountNumber + 1;
-    console.log(totalHeardCount);
-    document.getElementById('heart-Count').innerText = totalHeardCount;
+        e.preventDefault();
+        const heartCount = document.getElementById('heart-Count').innerText;
+        const heartCountNumber = parseInt(heartCount);
+        const totalHeardCount = heartCountNumber + 1;
+        console.log(totalHeardCount);
+        document.getElementById('heart-Count').innerText = totalHeardCount;
 
-})
+    })
 
 }
 
 // call section
 const callButton = document.getElementsByClassName('call-button');
-for(let i =0;i<callButton.length; i++){
+for (let i = 0; i < callButton.length; i++) {
 
 
     callButton[i].addEventListener('click', function (e) {
-    e.preventDefault();
-    console.log('clicked');
+        e.preventDefault();
 
-    const coinCount = parseInt(document.getElementById('coin-count').innerText);
-    console.log(coinCount);
-    if (coinCount < 20) {
-        alert('you have not available coin for calling');
-        return;
-    }
+        const dataElement = e.target.parentElement;
 
-    const data = {
-        name: 'sujon',
-        number:'555',
-        time: new Date().toLocaleTimeString("en-US", { hour12: true })
-    }
-    storeData.push(data);
+        console.log('clicked');
 
-    const addHistory = document.getElementById('add-history');
-    addHistory.innerText = "";
-    for (const data of storeData) {
-        const div = document.createElement("div");
-        div.innerHTML = `
+        const coinCount = parseInt(document.getElementById('coin-count').innerText);
+        console.log(coinCount);
+
+        if (coinCount < 20) {
+            alert('you have not coin for calling');
+            return;
+        }
+        const callButton = dataElement.querySelector('.call-button');
+        console.log(callButton);
+
+        const protishthan = callButton.dataset.protithan;
+        const number = callButton.dataset.number;
+        const service = callButton.dataset.service;
+
+        const data = {
+            name: protishthan,
+            number: number,
+            service: service,
+            time: new Date().toLocaleTimeString("en-US", { hour12: true })
+        }
+        storeData.push(data);
+
+        alert(`📞 Calling ${data.service} Service ${data.number}`);
+
+        const addHistory = document.getElementById('add-history');
+        addHistory.innerText = "";
+        for (const data of storeData) {
+            const div = document.createElement("div");
+            div.innerHTML = `
         <div class="flex justify-between items-center bg-[#FFE3E2] p-5 rounded-2xl mt-3">
                             <div>
                                 <h1 class="font-extrabold text-[12px]">${data.name}</h1>
@@ -52,18 +65,31 @@ for(let i =0;i<callButton.length; i++){
                             <p class="text-[12px]">${data.time}</p>
                         </div>
         `
-        addHistory.appendChild(div);
-    }
+            addHistory.appendChild(div);
+        }
 
-    const totalCoinCount = coinCount - 20;
-    document.getElementById('coin-count').innerText = totalCoinCount;
+        const totalCoinCount = coinCount - 20;
+        document.getElementById('coin-count').innerText = totalCoinCount;
 
-});
+    });
 }
 
 
 // call history clear button
-document.getElementById('clearAll-button').addEventListener('click',function(){
-    const removeHistoryContainer = document.getElementById('add-history').style.display= "none";
-
+document.getElementById('clearAll-button').addEventListener('click', function () {
+    const removeHistoryContainer = document.getElementById('add-history').style.display = "none";
 })
+
+
+const copyCountTracker = document.getElementById('copy-count');
+
+const copyButton = document.querySelectorAll(".btn-copy");
+for (let i = 0; i < copyButton.length; i++) {
+    copyButton[i].addEventListener('click', async function (e) {
+        const parentElement = e.target.parentElement;
+        const callButton = parentElement.querySelector('.call-button');
+        const number = callButton.dataset.number;
+        await navigator.clipboard.writeText(number);
+        copyCountTracker.textContent = `${Number(copyCountTracker.textContent) + 1}`;
+    })
+}
